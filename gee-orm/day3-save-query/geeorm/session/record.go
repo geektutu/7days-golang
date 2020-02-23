95814ae9ae52dc1b446f7506189eede16685dd78
gzdaijie <gzdaijie@163.com>
2020-02-24 01:24:05 +0800

    day3 add find multi records & find first record

--- gee-orm/day3-save-query/geeorm/session/record.go
+++ gee-orm/day3-save-query/geeorm/session/record.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"fmt"
+	"reflect"
 	"strings"
 )
 
@@ -32,12 +33,49 @@ func (s *Session) Create(values ...interface{}) (int64, error) {
 	return result.RowsAffected()
 }
 
+// First gets the 1st row
 func (s *Session) First(value interface{}) error {
 	table := s.RefTable(value)
+
 	fieldSQL := strings.Join(table.FieldNames, ", ")
-	sql := fmt.Sprintf("SELECT (%v) FROM %s LIMIT 1", fieldSQL, table.TableName)
+	selectSQL := fmt.Sprintf("SELECT %v FROM %s LIMIT 1", fieldSQL, table.TableName)
+	row := s.Raw(selectSQL).QueryRow()
 
-	row := s.Raw(sql).QueryRow()
+	dest := reflect.ValueOf(value).Elem()
+	var values []interface{}
+	for _, name := range table.FieldNames {
+		values = append(values, dest.FieldByName(name).Addr().Interface())
+	}
 
-	return row.Scan(value)
+	return row.Scan(values...)
+}
+
+// Find gets all eligible records
+func (s *Session) Find(values interface{}) error {
+	destSlice := reflect.Indirect(reflect.ValueOf(values))
+	destType := destSlice.Type().Elem()
+	table := s.RefTable(reflect.New(destType).Elem().Interface())
+
+	fieldSQL := strings.Join(table.FieldNames, ", ")
+	selectSQL := fmt.Sprintf("SELECT %v FROM %s", fieldSQL, table.TableName)
+	rows, err := s.Raw(selectSQL).QueryRows()
+	if err != nil {
+		return err
+	}
+
+	for rows.Next() {
+		dest := reflect.New(destType).Elem()
+		var values []interface{}
+		for _, name := range table.FieldNames {
+			values = append(values, dest.FieldByName(name).Addr().Interface())
+		}
+		if err := rows.Scan(values...); err != nil {
+			return err
+		}
+		destSlice.Set(reflect.Append(destSlice, dest))
+	}
+	if err := rows.Close(); err != nil {
+		return err
+	}
+	return nil
 }
--- gee-orm/day3-save-query/geeorm/session/record_test.go
+++ gee-orm/day3-save-query/geeorm/session/record_test.go
@@ -14,3 +14,24 @@ func TestSession_Create(t *testing.T) {
 		t.Fatal("failed to create record")
 	}
 }
+
+func TestSession_First(t *testing.T) {
+	_ = NewSession().DropTable(&User{})
+	_ = NewSession().CreateTable(&User{})
+	_, _ = NewSession().Create(user1)
+	u := &User{}
+	err := NewSession().First(u)
+	if err != nil || u.Age != user1.Age || u.Name != user1.Name {
+		t.Fatal("failed to query first")
+	}
+}
+
+func TestSession_Find(t *testing.T) {
+	_ = NewSession().DropTable(&User{})
+	_ = NewSession().CreateTable(&User{})
+	_, _ = NewSession().Create(user1, user2)
+	users := []User{}
+	if err := NewSession().Find(&users); err != nil || len(users) != 2 {
+		t.Fatal("failed to query all")
+	}
+}
