a28737e42fd8ab51f74f78732eebfd2652aaefa1
gzdaijie <gzdaijie@163.com>
2020-02-26 22:16:13 +0800

    add day6 transaction

---
+++ gee-orm/day6-transaction/geeorm.go
@@ -0,0 +1,75 @@
+package geeorm
+
+import (
+	"database/sql"
+	"geeorm/dialect"
+	"geeorm/log"
+	"geeorm/session"
+)
+
+// Engine is the main struct of geeorm, manages all db sessions and transactions.
+type Engine struct {
+	db      *sql.DB
+	dialect dialect.Dialect
+}
+
+// NewEngine create a instance of Engine
+// connect database and ping it to test whether it's alive
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	// Send a ping to make sure the database connection is alive.
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	// make sure the specific dialect exists
+	dial, ok := dialect.GetDialect(driver)
+	if !ok {
+		log.Errorf("dialect %s Not Found", driver)
+		return
+	}
+	e = &Engine{db: db, dialect: dial}
+	log.Info("Connect database success")
+	return
+}
+
+// Close database connection
+func (e *Engine) Close() (err error) {
+	if err = e.db.Close(); err == nil {
+		log.Info("Close database success")
+	}
+	return
+}
+
+// NewSession creates a new session for next operations
+func (e *Engine) NewSession() *session.Session {
+	return session.New(e.db, e.dialect)
+}
+
+// TxFunc will be called between tx.Begin() and tx.Commit()
+// https://stackoverflow.com/questions/16184238/database-sql-tx-detecting-commit-or-rollback
+type TxFunc func(*session.Session) (interface{}, error)
+
+// Transaction executes sql wrapped in a transaction, then automatically commit if no error occurs
+func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
+	s := e.NewSession()
+	if err := s.Begin(); err != nil {
+		return nil, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = s.Rollback()
+			panic(p) // re-throw panic after Rollback
+		} else if err != nil {
+			_ = s.Rollback() // err is non-nil; don't change it
+		} else {
+			err = s.Commit() // err is nil; if Commit returns error update err
+		}
+	}()
+
+	return f(s)
+}
