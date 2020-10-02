b9f5ca1bb262ecfaf5275478192b763714028144
gzdaijie <gzdaijie@gmail.com>
2020-10-02 17:25:53 +0800

    fix service numCalls read issue

--- gee-rpc/day3-service/service_test.go
+++ gee-rpc/day3-service/service_test.go
@@ -15,7 +15,7 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-// it's not a exported method
+// it's not a exported Method
 func (f Foo) sum(args Args, reply *int) error {
 	*reply = args.Num1 + args.Num2
 	return nil
@@ -30,9 +30,9 @@ func _assert(condition bool, msg string, v ...interface{}) {
 func TestNewService(t *testing.T) {
 	var foo Foo
 	s := newService(&foo)
-	_assert(len(s.method) == 1, "wrong service method, expect 1, but got %d", len(s.method))
+	_assert(len(s.method) == 1, "wrong service Method, expect 1, but got %d", len(s.method))
 	mType := s.method["Sum"]
-	_assert(mType != nil, "wrong method, Sum shouldn't nil")
+	_assert(mType != nil, "wrong Method, Sum shouldn't nil")
 }
 
 func TestMethodType_Call(t *testing.T) {
@@ -44,5 +44,5 @@ func TestMethodType_Call(t *testing.T) {
 	replyv := mType.newReplyv()
 	argv.Set(reflect.ValueOf(Args{Num1: 1, Num2: 3}))
 	err := s.call(mType, argv, replyv)
-	_assert(err == nil && *replyv.Interface().(*int) == 4 && mType.numCalls == 1, "failed to call Foo.Sum")
+	_assert(err == nil && *replyv.Interface().(*int) == 4 && mType.NumCalls() == 1, "failed to call Foo.Sum")
 }
