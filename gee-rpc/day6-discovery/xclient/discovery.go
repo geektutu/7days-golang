366f09aa6b0bf1095a9d7c7aa380276ed403c7cf
gzdaijie <gzdaijie@gmail.com>
2020-10-05 11:48:48 +0800

    gee-rpc day6 add robin select for discovery

--- gee-rpc/day6-discovery/xclient/discovery.go
+++ gee-rpc/day6-discovery/xclient/discovery.go
@@ -10,7 +10,7 @@ type SelectMode int
 
 const (
 	RandomSelect SelectMode = iota // select randomly
-	RobbinSelect                   // select using Robbin algorithm, not implemented
+	RobinSelect                    // select using Robbin algorithm
 )
 
 type Discovery interface {
@@ -26,6 +26,7 @@ type MultiServersDiscovery struct {
 	r       *rand.Rand   // generate random number
 	mu      sync.RWMutex // protect following
 	servers []string
+	index   int // record the selected position for robin algorithm
 }
 
 // Update the servers of discovery dynamically if needed
@@ -36,14 +37,18 @@ func (d *MultiServersDiscovery) Update(servers []string) {
 }
 
 func (d *MultiServersDiscovery) Get(mode SelectMode) string {
-	d.mu.RLock()
-	defer d.mu.RUnlock()
+	d.mu.Lock()
+	defer d.mu.Unlock()
 	if len(d.servers) == 0 {
 		return ""
 	}
 	switch mode {
 	case RandomSelect:
 		return d.servers[d.r.Intn(len(d.servers))]
+	case RobinSelect:
+		s := d.servers[d.index]
+		d.index = (d.index + 1) % len(d.servers)
+		return s
 	default:
 		return ""
 	}
