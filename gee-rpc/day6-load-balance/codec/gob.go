298ce560a496da66c93c1d3f80e9650ee89720b8
gzdaijie <gzdaijie@gmail.com>
2020-10-07 00:49:17 +0800

    gee-rpc: gob.go use err = to replace err :=

--- gee-rpc/day6-load-balance/codec/gob.go
+++ gee-rpc/day6-load-balance/codec/gob.go
@@ -41,15 +41,15 @@ func (c *GobCodec) Write(h *Header, body interface{}) (err error) {
 			_ = c.Close()
 		}
 	}()
-	if err := c.enc.Encode(h); err != nil {
+	if err = c.enc.Encode(h); err != nil {
 		log.Println("rpc: gob error encoding header:", err)
-		return err
+		return
 	}
-	if err := c.enc.Encode(body); err != nil {
+	if err = c.enc.Encode(body); err != nil {
 		log.Println("rpc: gob error encoding body:", err)
-		return err
+		return
 	}
-	return nil
+	return
 }
 
 func (c *GobCodec) Close() error {
