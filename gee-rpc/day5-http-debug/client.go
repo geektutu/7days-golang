d958ec5deecfb08c3c6006356723ecbbd658f87a
gzdaijie <gzdaijie@gmail.com>
2020-10-06 19:44:59 +0800

    geerpc/day5 day6 day7: update the function description of XDial

--- gee-rpc/day5-http-debug/client.go
+++ gee-rpc/day5-http-debug/client.go
@@ -306,7 +306,9 @@ func DialHTTP(network, address string, opts ...*Option) (*Client, error) {
 	return dialTimeout(dialHTTP, network, address, opts...)
 }
 
-// XDial use a general format to represent a rpc server
+// XDial calls different functions to connect to a RPC server
+// according the first parameter rpcAddr.
+// rpcAddr is a general format (protocol@addr) to represent a rpc server
 // eg, http@10.0.0.1:7001, tcp@10.0.0.1:9999, unix@/tmp/geerpc.sock
 func XDial(rpcAddr string, opts ...*Option) (*Client, error) {
 	parts := strings.Split(rpcAddr, "@")
