8076dacd0c7228e78db7c7793701f20248714fe6
stevenshuang <stevenshuang521@gmail.com>
2021-02-22 00:13:55 +0800

    url动态参数与固定参数冲突

--- gee-web/day3-router/main.go
+++ gee-web/day3-router/main.go
@@ -48,6 +48,10 @@ func main() {
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 	})
 
+	r.GET("/hello/cool", func(c *gee.Context) {
+		c.String(http.StatusOK, "fix pattern, you're at %s\n", c.Path)
+	})
+
 	r.GET("/assets/*filepath", func(c *gee.Context) {
 		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
 	})
