7f8715f392e082bfe8ff6051b95755a86e37b324
zhangbb <854641898@qq.com>
2021-12-12 20:11:45 +0800

    perf:提供一个更能体现Do()作用的test case

--- gee-cache/day7-proto-buf/geecache/singleflight/singleflight_test.go
+++ gee-cache/day7-proto-buf/geecache/singleflight/singleflight_test.go
@@ -1,11 +1,14 @@
 package singleflight
 
 import (
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestDo(t *testing.T) {
 	var g Group
+
 	v, err := g.Do("key", func() (interface{}, error) {
 		return "bar", nil
 	})
@@ -13,4 +16,30 @@ func TestDo(t *testing.T) {
 	if v != "bar" || err != nil {
 		t.Errorf("Do v = %v, error = %v", v, err)
 	}
+}
+
+func TestGoDo(t *testing.T) {
+	var g Group
+	wg := sync.WaitGroup{}
+
+	times := 10
+	wg.Add(times)
+	for i := 0; i < times; i++ {
+		go func() interface{} {
+			v, err := g.Do("key", func() (interface{}, error) {
+				time.Sleep(time.Millisecond * 50)
+				return "bar", nil
+			})
+
+			wg.Done()
+
+			if v != "bar" || err != nil {
+				t.Errorf("Do v = %v, error = %v", v, err)
+			}
+
+			return 1
+		}()
+	}
+
+	wg.Wait()
 }
