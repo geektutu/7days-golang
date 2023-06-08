2bcb48d91cfe718d2a67238833072a09aa5b2426
Alcuin <41372488+Alcuin1@users.noreply.github.com>
2023-06-08 15:33:03 +0800

    Day2 单机并发缓存geecache.go

--- gee-cache/day2-single-node/geecache/geecache.go
+++ gee-cache/day2-single-node/geecache/geecache.go
@@ -7,21 +7,33 @@ import (
 )
 
 // A Group is a cache namespace and associated data loaded spread over
+// 一个 Group 可以认为是一个缓存的命名空间
 type Group struct {
-	name      string
-	getter    Getter
+	// 每个 Group 拥有一个唯一的名称 name。比如可以创建三个 Group，
+	// 缓存学生的成绩命名为 scores，缓存学生信息的命名为 info，缓存学生课程的命名为 courses。
+	name string
+	// 第二个属性是 getter Getter，即缓存未命中时获取源数据的回调(callback)。
+	getter Getter
+	// 第三个属性是 mainCache cache，即一开始实现的并发缓存
 	mainCache cache
 }
 
 // A Getter loads data for a key.
+// 定义接口 Getter 和 回调函数 Get(key string)([]byte, error)，参数是 key，返回值是 []byte。
 type Getter interface {
 	Get(key string) ([]byte, error)
 }
 
 // A GetterFunc implements Getter with a function.
+// 定义函数类型 GetterFunc，并实现 Getter 接口的 Get 方法。
 type GetterFunc func(key string) ([]byte, error)
 
 // Get implements Getter interface function
+// 函数类型实现某一个接口，称之为接口型函数，方便使用者在调用时既能够传入函数作为参数，也能够传入实现了该接口的结构体作为参数。
+// 补充：
+// 这里呢，定义了一个接口 Getter，只包含一个方法 Get(key string) ([]byte, error)，紧接着定义了一个函数类型 GetterFunc，
+// GetterFunc 参数和返回值与 Getter 中 Get 方法是一致的。而且 GetterFunc 还定义了 Get 方式，并在 Get 方法中调用自己，
+// 这样就实现了接口 Getter。所以 GetterFunc 是一个实现了接口的函数类型，简称为接口型函数。
 func (f GetterFunc) Get(key string) ([]byte, error) {
 	return f(key)
 }
@@ -32,6 +44,7 @@ var (
 )
 
 // NewGroup create a new instance of Group
+// 构建函数 NewGroup 用来实例化 Group，并且将 group 存储在全局变量 groups 中。
 func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 	if getter == nil {
 		panic("nil Getter")
@@ -52,28 +65,32 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 func GetGroup(name string) *Group {
 	mu.RLock()
 	g := groups[name]
+	// GetGroup 用来特定名称的 Group，这里使用了只读锁 RLock()，因为不涉及任何冲突变量的写操作
 	mu.RUnlock()
 	return g
 }
 
 // Get value for a key from cache
+// Get 方法实现了上述所说的流程 ⑴ 和 ⑶。
 func (g *Group) Get(key string) (ByteView, error) {
 	if key == "" {
 		return ByteView{}, fmt.Errorf("key is required")
 	}
-
+	// 流程 ⑴ ：从 mainCache 中查找缓存，如果存在则返回缓存值
 	if v, ok := g.mainCache.get(key); ok {
 		log.Println("[GeeCache] hit")
 		return v, nil
 	}
-
+	// 流程 ⑶ ：缓存不存在，则调用 load 方法
 	return g.load(key)
 }
 
+// load 调用 getLocally（分布式场景下会调用 getFromPeer 从其他节点获取），
 func (g *Group) load(key string) (value ByteView, err error) {
 	return g.getLocally(key)
 }
 
+// getLocally 调用用户回调函数 g.getter.Get() 获取源数据，并且将源数据添加到缓存 mainCache 中（通过 populateCache 方法）
 func (g *Group) getLocally(key string) (ByteView, error) {
 	bytes, err := g.getter.Get(key)
 	if err != nil {
