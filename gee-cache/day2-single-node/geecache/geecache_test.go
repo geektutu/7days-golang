fd3278efa07e32d1ce13540c3b9989ef0f61c778
Alcuin <41372488+Alcuin1@users.noreply.github.com>
2023-06-08 15:42:13 +0800

    Day2 单机并发缓存 geecache_test.go

--- gee-cache/day2-single-node/geecache/geecache_test.go
+++ gee-cache/day2-single-node/geecache/geecache_test.go
@@ -13,12 +13,13 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
+// 在这个测试用例中，我们借助 GetterFunc 的类型转换，将一个匿名回调函数转换成了接口 f Getter。
 func TestGetter(t *testing.T) {
 	var f Getter = GetterFunc(func(key string) ([]byte, error) {
 		return []byte(key), nil
 	})
-
 	expect := []byte("key")
+	// 调用该接口的方法 f.Get(key string)，实际上就是在调用匿名回调函数。
 	if v, _ := f.Get("key"); !reflect.DeepEqual(v, expect) {
 		t.Fatal("callback failed")
 	}
@@ -29,10 +30,13 @@ func TestGet(t *testing.T) {
 	gee := NewGroup("scores", 2<<10, GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
+			// 如果存在db中
 			if v, ok := db[key]; ok {
+				// 如果该key没有调用回调函数,就初始化一下
 				if _, ok := loadCounts[key]; !ok {
 					loadCounts[key] = 0
 				}
+				// count++
 				loadCounts[key]++
 				return []byte(v), nil
 			}
@@ -43,6 +47,7 @@ func TestGet(t *testing.T) {
 		if view, err := gee.Get(k); err != nil || view.String() != v {
 			t.Fatal("failed to get value of Tom")
 		}
+		// 在缓存已经存在的情况下，是否直接从缓存中获取，为了实现这一点，使用 loadCounts 统计某个键调用回调函数的次数，如果次数大于1，则表示调用了多次回调函数，没有缓存。
 		if _, err := gee.Get(k); err != nil || loadCounts[k] > 1 {
 			t.Fatalf("cache %s miss", k)
 		}
