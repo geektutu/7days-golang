3c0a79366de7a4a293097ab985eb1a05f17330f4
Alcuin <41372488+Alcuin1@users.noreply.github.com>
2023-06-08 16:56:55 +0800

    Day5 分布式节点 peers.go

--- gee-cache/day5-multi-nodes/geecache/peers.go
+++ gee-cache/day5-multi-nodes/geecache/peers.go
@@ -2,11 +2,13 @@ package geecache
 
 // PeerPicker is the interface that must be implemented to locate
 // the peer that owns a specific key.
+// PeerPicker 的 PickPeer() 方法用于根据传入的 key 选择相应节点 PeerGetter。
 type PeerPicker interface {
 	PickPeer(key string) (peer PeerGetter, ok bool)
 }
 
 // PeerGetter is the interface that must be implemented by a peer.
+// 接口 PeerGetter 的 Get() 方法用于从对应 group 查找缓存值。PeerGetter 就对应于上述流程中的 HTTP 客户端
 type PeerGetter interface {
 	Get(group string, key string) ([]byte, error)
 }
