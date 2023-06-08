a668fe12668adad47df82d6c45704be04d425695
Alcuin <41372488+Alcuin1@users.noreply.github.com>
2023-06-08 16:06:07 +0800

    Day3 HTTP 服务端 http.go

--- gee-cache/day3-http-server/geecache/http.go
+++ gee-cache/day3-http-server/geecache/http.go
@@ -31,32 +31,33 @@ func (p *HTTPPool) Log(format string, v ...interface{}) {
 
 // ServeHTTP handle all http requests
 func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// ServeHTTP 的实现逻辑是比较简单的，首先判断访问路径的前缀是否是 basePath，不是返回错误。
 	if !strings.HasPrefix(r.URL.Path, p.basePath) {
 		panic("HTTPPool serving unexpected path: " + r.URL.Path)
 	}
 	p.Log("%s %s", r.Method, r.URL.Path)
 	// /<basepath>/<groupname>/<key> required
+	// 我们约定访问路径格式为 /<basepath>/<groupname>/<key>
 	parts := strings.SplitN(r.URL.Path[len(p.basePath):], "/", 2)
 	if len(parts) != 2 {
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
-
 	groupName := parts[0]
 	key := parts[1]
-
+	// 通过 groupname 得到 group 实例
 	group := GetGroup(groupName)
 	if group == nil {
 		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
 		return
 	}
-
+	// 再使用 group.Get(key) 获取缓存数据
 	view, err := group.Get(key)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
 	w.Header().Set("Content-Type", "application/octet-stream")
+	// 最终使用 w.Write() 将缓存值作为 httpResponse 的 body 返回
 	w.Write(view.ByteSlice())
 }
