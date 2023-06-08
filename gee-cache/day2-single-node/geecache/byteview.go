a5f414e966fc900964fc1a16d82689d733427701
Alcuin <41372488+Alcuin1@users.noreply.github.com>
2023-06-08 15:40:30 +0800

    Day2 单机并发缓存 byteview.go

--- gee-cache/day2-single-node/geecache/byteview.go
+++ gee-cache/day2-single-node/geecache/byteview.go
@@ -1,16 +1,19 @@
 package geecache
 
 // A ByteView holds an immutable view of bytes.
+// ByteView 只有一个数据成员，b []byte，b 将会存储真实的缓存值。选择 byte 类型是为了能够支持任意的数据类型的存储，例如字符串、图片等。
 type ByteView struct {
 	b []byte
 }
 
 // Len returns the view's length
+// 实现 Len() int 方法，我们在 lru.Cache 的实现中，要求被缓存对象必须实现 Value 接口，即 Len() int 方法，返回其所占的内存大小。
 func (v ByteView) Len() int {
 	return len(v.b)
 }
 
 // ByteSlice returns a copy of the data as a byte slice.
+// b 是只读的，使用 ByteSlice() 方法返回一个拷贝，防止缓存值被外部程序修改。
 func (v ByteView) ByteSlice() []byte {
 	return cloneBytes(v.b)
 }
