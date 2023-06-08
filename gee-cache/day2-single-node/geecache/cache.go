4815d603f9530b9de440fbaa2f4d82b58b00a5b8
Alcuin <41372488+Alcuin1@users.noreply.github.com>
2023-06-08 15:37:52 +0800

    Day1 LRU 缓存淘汰策略 cache.go

--- gee-cache/day2-single-node/geecache/cache.go
+++ gee-cache/day2-single-node/geecache/cache.go
@@ -5,12 +5,15 @@ import (
 	"sync"
 )
 
+// cache.go 的实现非常简单，实例化 lru，封装 get 和 add 方法，并添加互斥锁 mu。
 type cache struct {
 	mu         sync.Mutex
 	lru        *lru.Cache
 	cacheBytes int64
 }
 
+// 在 add 方法中，判断了 c.lru 是否为 nil，如果等于 nil 再创建实例。这种方法称之为延迟初始化(Lazy Initialization)，
+// 一个对象的延迟初始化意味着该对象的创建将会延迟至第一次使用该对象时。主要用于提高性能，并减少程序内存要求。
 func (c *cache) add(key string, value ByteView) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
